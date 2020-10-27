6f8bd7119ccb508dc841665dd355c199f5cf95f6
kong0827 <1351882069@qq.com>
2020-10-28 00:28:41 +0800

    supplier接口

---
+++ src/main/java/function/Example03_Supplier.java
@@ -0,0 +1,23 @@
+package function;
+
+import java.util.function.Supplier;
+
+/**
+ * @author kxj
+ * @date 2020/10/28 0:16
+ * @desc java.util.function.Supplier<T> 接口中只包含一个无参的方法：T get(),用来获取一个泛型参数指定类型的对象数据。
+ * 由于是一个函数式接口，意味着对应的Lambda表达式需要对外提供一个符合泛型类型的对象数据
+ */
+public class Example03_Supplier {
+    public static void main(String[] args) {
+        String msgA = "Hello";
+        String msgB = "Java";
+        String s = getString(() -> msgA + msgB);
+        System.out.println(s);
+    }
+
+    private static String getString(Supplier<String> function) {
+        return function.get();
+    }
+}
+
---
+++ src/main/java/function/Example04_Supplier.java
@@ -0,0 +1,32 @@
+package function;
+
+import java.util.function.Supplier;
+
+/**
+ * @author kxj
+ * @date 2020/10/28 0:24
+ * @desc
+ *
+ * 使用Supplier接口作为方法的参数类型，通过Lambda表达式求int数组中的最大值，
+ */
+public class Example04_Supplier {
+
+    public static void main(String[] args) {
+        int[] arr = {1, 4, 7, 3, 2, 8, 6};
+
+        int maxNumber = getMaxNumber(() -> {
+            int max = arr[0];
+            for (int i : arr) {
+                if (i > max) {
+                    max = i;
+                }
+            }
+            return max;
+        });
+        System.out.println(maxNumber);
+    }
+
+    private static int getMaxNumber(Supplier<Integer> supplier) {
+        return supplier.get();
+    }
+}
