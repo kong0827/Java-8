e7cc8cfaf7ba2b3aa58c96d10f1a3bc9a6bf9a72
kongxiangjin <1351882069@qq.com>
2020-12-20 19:15:11 +0800

    map合并

--- src/main/java/stream/MapSortTest.java
+++ src/main/java/stream/MapSortTest.java
@@ -1,5 +1,6 @@
 package stream;
 
+import entity.Employee;
 import org.junit.Test;
 
 import java.util.HashMap;
@@ -74,5 +75,56 @@ public class MapSortTest {
         codes.put("Pakistan", 92);
         Map<String, Integer> sortedMap = new TreeMap<>(codes);
         sortedMap.entrySet().forEach(System.out::println);
+    }
+
+    @Test
+    public void testMerge() {
+        Map<String, Employee> map1 = new HashMap<String, Employee>();
+        Employee employee1 = new Employee();
+        employee1.setAge(1);
+        employee1.setLastName("小明");
+
+        Employee employee2 = new Employee();
+        employee2.setAge(1);
+        employee2.setLastName("小张");
+
+        map1.put("小明", employee1);
+        map1.put(employee2.getLastName(), employee2);
+
+
+        Map<String, Employee> map2 = new HashMap<String, Employee>();
+        Employee employee3 = new Employee();
+        employee3.setAge(2);
+        employee3.setLastName("小明");
+
+        Employee employee4 = new Employee();
+        employee4.setAge(1);
+        employee4.setLastName("小强");
+
+        map2.put(employee3.getLastName(), employee3);
+        map2.put(employee4.getLastName(), employee4);
+
+        Map<String, Employee> result = Stream.concat(map1.entrySet().stream(), map2.entrySet().stream())
+                .collect(Collectors.toMap(
+                        Map.Entry::getKey,
+                        Map.Entry::getValue,
+                        (value1, value2) -> {
+
+                            Employee employee = new Employee();
+                            int age = value2.getAge();
+                            if (value1.getAge() >= value2.getAge()) {
+                                age = value1.getAge();
+                            }
+                            employee.setLastName(value1.getLastName());
+                            employee.setAge(age);
+
+                            return employee;
+                        }
+                ));
+
+
+        System.out.println(result);
+
+
     }
 }
